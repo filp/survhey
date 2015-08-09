66593f04cbb0163ede3a2c72a09470ca2779e342
Filipe Dobreira <dobreira@gmail.com>
2015-08-09 18:23:30 +0100

    Redirect to apex

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :redirect_to_apex if Rails.env.production?
+
+private
+
+  def redirect_to_apex
+    if request.host =~ /\Awww\./
+      redirect_to request.host[4..-1] + request.fullpath, status: :moved_permanently
+    end
+  end
 end
