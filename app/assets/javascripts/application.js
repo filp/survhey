79a459df8b1645fa27ab630f70ef10b320a53de0
Filipe Dobreira <dobreira@gmail.com>
2015-08-10 11:11:15 +0100

    Cleanup account linking header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,8 +7,8 @@
 
 $(function () {
 
+  // If the results graph is present on the page, render it:
   var canvas = document.getElementById("results-graph");
-
   if (canvas) {
     var ctx = canvas.getContext("2d");
     var data = JSON.parse($(canvas).attr("data-graph"));
@@ -19,4 +19,9 @@ $(function () {
       responsive: true
     });
   }
+
+  // Select the contents of the account link input field when it's focused:
+  $(".account-link-url").click(function () {
+    $(this).select();
+  });
 });
