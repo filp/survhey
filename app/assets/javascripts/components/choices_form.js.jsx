93a287dbe9048ba3d122327962855c5cae7c9de8
Filipe Dobreira <dobreira@gmail.com>
2015-08-13 16:49:39 +0100

    Add ChoicesForm

---
+++ app/assets/javascripts/components/choices_form.js.jsx
@@ -0,0 +1,53 @@
+window.Survhey.ChoicesForm = (function () {
+  var CHOICES_LIMIT = 25;
+  var PLACEHOLDER = "Write the first choice here";
+
+  var ChoicesForm = React.createClass({
+    getInitialState: function () {
+      return {
+        choices: 1
+      };
+    },
+
+    disableEnter: function (event) {
+
+      if (event.which === 13) {
+        event.preventDefault();
+      }
+    },
+
+    updateChoices: function (id) {
+      var num = this.state.choices;
+
+      if (id + 1 == num && num < CHOICES_LIMIT) {
+        this.setState({ choices: num + 1 });
+      }
+    },
+
+    choices: function () {
+      var choices = [];
+
+      for (var id = 0; id < this.state.choices; id++) {
+        var formKey = "survey[choices_attributes][" + id + "][body]";
+        var placeholder = id === 0 && PLACEHOLDER || "";
+
+        choices.push(
+          <input type="text" key={id} name={formKey} className="text-input"
+                 onChange={this.updateChoices.bind(this, id)} placeholder={placeholder} onKeyDown={this.disableEnter} />
+        );
+      }
+
+      return choices;
+    },
+
+    render: function () {
+      return (
+        <div className="survey__choices-form">
+          {this.choices()}
+        </div>
+      );
+    }
+  });
+
+  return ChoicesForm;
+}());
